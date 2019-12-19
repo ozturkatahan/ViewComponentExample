5c425a22be6ce9d3000388b967ea344ce09e3f0d
atahan.ozturk <ozturkatahan5@gmail.com>
2019-12-19 20:36:41 +0300

    Add project files.

---
+++ ViewComponentGiris/ViewComponents/CategoriesListViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ViewComponentGiris.Models;
+
+namespace ViewComponentGiris.ViewComponents
+{
+    public class CategoriesListViewComponent : ViewComponent
+    {
+        private readonly Database db;
+        public CategoriesListViewComponent(Database database)
+        {
+            db = database;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            Database db = new Database();
+
+            return View(db.GetCategories());
+        }
+    }
+}
